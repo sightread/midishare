20525bf76fa3928a5b40d96258298c0dd010f396
Jake F - @samouri <samouri@users.noreply.github.com>
2021-11-28 21:16:47 -0800

    tune up the UploadDialog

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,45 +3,54 @@ export type SearchBoxProps = { onSearch: (val: string) => void }
 
 export function Search({ onSearch }: SearchBoxProps) {
   return (
-    <div style={{ position: "relative", height: 50, width: "100%", display: "flex" }}>
-      <input
-        type="search"
-        onChange={(e: any) => onSearch(e.target.value)}
-        style={{
-          width: "100%",
-          height: "100%",
-          paddingLeft: 20,
-          paddingRight: 40,
-          backgroundColor: "#F2F4F8",
-          borderTopLeftRadius: 12,
-          borderBottomLeftRadius: 12,
-          boxShadow: "inset 0px 1px 4px rgba(0, 0, 0, 0.25)",
-          border: "none",
-        }}
-        placeholder="Search for music"
-      />
-      <div
-        style={{
-          position: "relative",
-          right: 0,
-          height: "100%",
-          backgroundColor: "#DDDDDD",
-          borderTopRightRadius: 12,
-          borderBottomRightRadius: 12,
-          width: 60,
-        }}
-      >
-        <SearchIcon
-          height={30}
-          width={30}
-          style={{
-            position: "absolute",
-            left: 13,
-            top: "50%",
-            transform: "translateY(-50%)",
-          }}
-        />
+    <>
+      <style jsx>{`
+        .container {
+          display: flex;
+          position: relative;
+          height: 50px;
+          width: 100%;
+        }
+
+        input {
+          width: 100%;
+          height: 100%;
+          padding-left: 20px;
+          padding-right: 40px;
+          background-color: #f2f4f8;
+          border-top-left-radius: 12px;
+          border-bottom-left-radius: 12px;
+          box-shadow: inset 0px 1px 4px rgba(0, 0, 0, 0.25);
+          border: none;
+        }
+
+        .icon-box {
+          position: relative;
+          right: 0;
+          height: 100%;
+          background-color: #ddd;
+          border-top-right-radius: 12px;
+          border-bottom-right-radius: 12px;
+          width: 60px;
+        }
+        /*TODO: why doesn't the jsx- class get applied to the svg? */
+        .icon {
+          position: absolute;
+          left: 10px;
+          top: 50%;
+          transform: translateY(-50%);
+        }
+      `}</style>
+      <div className="container">
+        <input type="search" onChange={(e: any) => onSearch(e.target.value)} placeholder="Search for music" />
+        <div className="icon-box">
+          <SearchIcon
+            height={30}
+            width={30}
+            style={{ position: "absolute", left: 10, top: "50%", transform: "translateY(-50%)" }}
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/components/UploadDialog.tsx
+++ src/components/UploadDialog.tsx
@@ -3,10 +3,16 @@ import { Dialog, DialogProps } from "./Dialog"
 
 export function UploadDialog(props: DialogProps) {
   return (
-    <Dialog {...props} aria-label="Upload Dialog" style={{ display: "flex", flexDirection: "column" }}>
+    <>
       <style jsx>{`
-        h1 {
+        .container {
+          display: flex;
+          flex-direction: column;
+        }
+
+        .title {
           font-weight: 100;
+          text-align: center;
         }
 
         .upload_box {
@@ -22,21 +28,72 @@ export function UploadDialog(props: DialogProps) {
         .upload_box button {
           width: 150px;
           height: 30px;
+        }
+
+        button {
           color: white;
           background-color: var(--primary);
           border-radius: 10px;
         }
+        .horizontal {
+          display: flex;
+          position: relative;
+          gap: 20px;
+        }
+        .form_inputs {
+          display: grid;
+          gap: 10px;
+          grid-template-rows: auto;
+          grid-template-columns: repeat(2, 1fr); /*first col should be min*/
+        }
       `}</style>
-      <h1>Upload a MIDI File</h1>
-      <div className="horizontal">
-        <div className="upload_box">
-          <button>Select file</button>
-          Or drag
+      <Dialog
+        {...props}
+        aria-label="Upload Dialog"
+        style={{ width: "calc(100vw-100px)", maxWidth: 600, minWidth: 400 }}
+      >
+        <div className="container">
+          <h1 className="title">Upload a MIDI File</h1>
+          <Spacer axis="vertical" size={24} />
+          <div className="horizontal">
+            <div className="upload_box">
+              <button>Select file</button>
+              Or drag
+            </div>
+            <div className="form_inputs">
+              <TextInput label="Title" />
+              <TextInput label="Artist" />
+              <SelectDialog label="Genre" options={["Jazz", "Pop"]} />
+            </div>
+          </div>
+          <Spacer size={64} axis="vertical" />
+          <button>Upload</button>
         </div>
-        <div className="select_dialogs"></div>
-      </div>
-      <Spacer size={64} axis="vertical" />
-      <button>Upload</button>
-    </Dialog>
+      </Dialog>
+    </>
+  )
+}
+
+type SelectDialogProps = { options: string[]; onSelect?: (val: string) => void; label: string }
+function SelectDialog({ options, label }: SelectDialogProps) {
+  return (
+    <>
+      <label>{label}</label>
+      <select>
+        {options.map((option) => (
+          <option>{option}</option>
+        ))}
+      </select>
+    </>
+  )
+}
+
+type TextInputProps = { onChange?: (str: string) => void; label: string }
+function TextInput({ label }: TextInputProps) {
+  return (
+    <>
+      <label>{label}</label>
+      <input type="text" />
+    </>
   )
 }
