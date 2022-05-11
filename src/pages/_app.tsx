c382b1ea1d772e08247f84356a4e4d5df4a5dd83
Jake <4656974+samouri@users.noreply.github.com>
2022-05-11 12:03:14 -0400

    Add GoogleAnalytics4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,9 +10,25 @@ import "features/pages/Landing/index.css"
 import "features/pages/Upload/index.css"
 
 import type { AppProps } from "next/app"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import * as gtag from "../lib/gtag"
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => gtag.pageview(url)
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
