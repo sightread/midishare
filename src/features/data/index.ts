a835a6feb09ec73f1f9e33fcbf37c5931fb0f42b
Jake <4656974+samouri@users.noreply.github.com>
2022-06-04 22:56:45 -0400

    SongAdd: Someone Like You

--- src/features/data/index.ts
+++ src/features/data/index.ts
@@ -179,6 +179,18 @@ export function getSongs(): SongMetadata[] {
       duration: "5:11",
       filename: "Passenger-Let_Her_Go",
     },
+    {
+      title: "Someone Like You",
+      artist: "Adele",
+      uploader: "sightread",
+      youtubeId: "GuQL84_5fX0",
+      uploadedAt: new Date("2022-06-04"),
+      originalSourceType: "musescore",
+      originalSourceUrl: "https://musescore.com/user/13543696/scores/4836661",
+      originalArranger: "matikavi11869",
+      duration: "4:50",
+      filename: "Adele-Someone_Like_You",
+    },
   ]
 }
 
