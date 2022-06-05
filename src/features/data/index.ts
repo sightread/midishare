3b432091dcea0971036a91cf2081fd5818330bb9
Jake <4656974+samouri@users.noreply.github.com>
2022-06-05 14:39:02 -0400

    remove One Final Effort dupe

--- src/features/data/index.ts
+++ src/features/data/index.ts
@@ -157,17 +157,6 @@ export function getSongs(): SongMetadata[] {
       duration: "3:28",
     },
     {
-      title: "Halo 3: One Final Effort",
-      artist: "Martin O' Donnell",
-      uploader: "sightread",
-      youtubeId: "MfFIfvNAWUY",
-      uploadedAt: new Date("2022-05-13"),
-      originalSourceType: "musescore",
-      originalSourceUrl: "https://musescore.com/user/40925/scores/5355836",
-      originalArranger: "rzhang1997",
-      duration: "3:28",
-    },
-    {
       title: "Let Her Go",
       artist: "Passenger",
       uploader: "sightread",
