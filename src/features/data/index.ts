ca98f3fc5f70362a9a9cbb1d09f744b1c8f5477c
Jake <4656974+samouri@users.noreply.github.com>
2022-06-09 10:19:11 -0700

    SongAdd: Franz_Lizt-La_Campanella

--- src/features/data/index.ts
+++ src/features/data/index.ts
@@ -216,6 +216,18 @@ export function getSongs(): SongMetadata[] {
       duration: "3:21",
       filename: "FKJ-Ylang_Ylang",
     },
+    {
+      title: "La Campanella",
+      artist: "Franz Liszt",
+      uploader: "sightread",
+      youtubeId: "a3QXkLiW6wc",
+      uploadedAt: new Date("2022-06-09"),
+      originalSourceType: "musescore",
+      originalSourceUrl: "https://musescore.com/classicman/scores/106022",
+      originalArranger: "ClassicMan",
+      duration: "4:02",
+      filename: "Franz_Liszt-La_Campanella",
+    },
   ]
 }
 
