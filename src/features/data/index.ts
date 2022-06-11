e8e82b8a70a0a41cd56c8b70e0505e375f02bd43
Jake <4656974+samouri@users.noreply.github.com>
2022-06-11 08:44:27 -0700

    SongAdd: Bieber stay

--- src/features/data/index.ts
+++ src/features/data/index.ts
@@ -240,6 +240,18 @@ export function getSongs(): SongMetadata[] {
       duration: "1:50",
       filename: "Ruth_B-Dandelions",
     },
+    {
+      title: "Stay",
+      artist: "Justin Bieber",
+      uploader: "sightread",
+      youtubeId: "y6WQ7E7Cj0k",
+      uploadedAt: new Date("2022-06-11"),
+      originalSourceType: "musescore",
+      originalSourceUrl: "https://musescore.com/user/32809872/scores/6863375",
+      originalArranger: "Mr. POP",
+      duration: "2:18",
+      filename: "Justin_Beiber-Stay",
+    },
   ]
 }
 
