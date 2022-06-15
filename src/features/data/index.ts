86be6d67cb2ce362f592888446cd1f9be00aef75
Jake <4656974+samouri@users.noreply.github.com>
2022-06-15 16:13:44 -0400

    SongAdd: BTS Yet To Come

--- src/features/data/index.ts
+++ src/features/data/index.ts
@@ -276,6 +276,18 @@ export function getSongs(): SongMetadata[] {
       duration: "2:32",
       filename: "Billy_Joel-Vienna_Vocal",
     },
+    {
+      title: "Yet To Come  (The Most Beautiful Moment)",
+      artist: "BTS (방탄소년단)",
+      uploader: "sightread",
+      youtubeId: "BPRXbYvy3IQ",
+      uploadedAt: new Date("2022-06-15"),
+      originalSourceType: "musescore",
+      originalSourceUrl: "https://musescore.com/user/23121456/scores/8193407",
+      originalArranger: "yx",
+      duration: "3:15",
+      filename: "BTS-Yet_To_Come",
+    },
   ]
 }
 
