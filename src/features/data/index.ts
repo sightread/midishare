a9ac19d66d3579e7288e01e706b5f6999c21d05b
Jake <4656974+samouri@users.noreply.github.com>
2022-06-21 21:47:43 -0400

    SongAdd: He's A Pirate

--- src/features/data/index.ts
+++ src/features/data/index.ts
@@ -367,8 +367,8 @@ export function getSongs(): SongMetadata[] {
       title: "He's A Pirate",
       artist: "Klaus Badelt",
       uploader: "sightread",
-      // youtubeId:
-      uploadedAt: new Date("2022-06-16"),
+      youtubeId: "pQ2OP7iC94c",
+      uploadedAt: new Date("2022-06-21"),
       originalSourceType: "musescore",
       originalSourceUrl: "https://musescore.com/user/2830596/scores/1421196",
       originalArranger: "onnikoivisto",
