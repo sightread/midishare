1d2e1a57f350bc70cb2ea4a85a6bd6e222d1975e
Jake <4656974+samouri@users.noreply.github.com>
2022-07-11 23:11:10 -0400

    SongAdd: Rick-Astley_Never_Gonna_Give_You_Up

---
+++ public/download/Rick-Astley_Never_Gonna_Give_You_Up/metadata.js
@@ -0,0 +1,11 @@
+export default {
+  title: "Never Gonna Give You Up",
+  artist: "Rick Astley",
+  uploader: "sightread",
+  youtubeId: "vqY_vpoTDM4",
+  uploadedAt: new Date("2022-07-11"),
+  originalSourceType: "musescore",
+  originalSourceUrl: "https://musescore.com/punctuationless/never-gonna-give-you-up",
+  originalArranger: "punctuationless",
+  duration: 204,
+}
