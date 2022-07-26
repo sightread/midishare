45e79535da7ec1c2a8d053f2d89e8d33c6130a14
Jake <4656974+samouri@users.noreply.github.com>
2022-07-26 09:54:24 -0400

    SongAdd: Pokemon Theme

---
+++ public/download/John_Siegler-Pokemon_Theme/metadata.js
@@ -0,0 +1,11 @@
+export default {
+  title: "Pokemon Theme",
+  artist: "John Siegler",
+  uploader: "sightread",
+  youtubeId: "Wjqau6ketHQ",
+  uploadedAt: new Date("2022-07-26"),
+  originalSourceType: "musescore",
+  originalSourceUrl: "https://musescore.com/user/1801321/scores/769686",
+  originalArranger: "Pimplup",
+  duration: 176,
+}
