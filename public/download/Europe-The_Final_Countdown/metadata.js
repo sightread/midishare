da61765e0bc285117a9f1bb87d838a6376bad4ff
Jake <4656974+samouri@users.noreply.github.com>
2022-07-27 14:13:11 -0400

    SongAdd: Europe - The Final Countdown

---
+++ public/download/Europe-The_Final_Countdown/metadata.js
@@ -0,0 +1,13 @@
+/** @type {import('../../../src/types').HandwrittenSongMetadata} */
+const metadata = {
+  title: "The Final Countdown (easy)",
+  artist: "Europe",
+  uploader: "sightread",
+  youtubeId: "2pn2i1k73zc",
+  uploadedAt: new Date("2022-07-27"),
+  originalSourceType: "musescore",
+  originalSourceUrl: "https://musescore.com/user/10383951/scores/2424421",
+  originalArranger: "Vinícius Kreisch",
+}
+
+export default metadata
