c7ce76ab9b323139f9613dd3f1d8bbaa3ff50ecf
Jake <4656974+samouri@users.noreply.github.com>
2022-08-19 15:06:24 -0400

    SongAdd: Armin Van Buuren - Hold On

---
+++ public/download/Armin_Van_Buuren-Hold_On/metadata.js
@@ -0,0 +1,10 @@
+export default {
+  title: "Hold On",
+  artist: "Armin Van Buuren",
+  youtubeId: "2SoSkXgdRtY",
+  uploader: "sightread",
+  uploadedAt: new Date("2022-08-19"),
+  originalSourceType: "musescore",
+  originalSourceUrl: "https://musescore.com/user/2423821/scores/7422989",
+  originalArranger: "veeroonaa",
+}
