8a06503f2d64113089a054af28574a01896e5075
Jake <4656974+samouri@users.noreply.github.com>
2022-10-26 01:43:45 -0400

    Upgrade to nextjs 13

--- src/components/MusicThumbnail.tsx
+++ src/components/MusicThumbnail.tsx
@@ -3,29 +3,28 @@ import { getYoutubeThumbnailUrl } from '@/features/data'
 import Link from 'next/link'
 import { Spacer } from './Spacer'
 import { cx } from '@/features/utils'
+import Image from 'next/image'
 
 export function MusicThumbnail({ metadata }: { metadata: SongMetadata }) {
   return (
     <div className={cx('transition-transform', 'hover:-translate-y-1', 'shadow-lg', 'rounded-b-lg')}>
       <Link href={`/detail/${metadata.id}`}>
-        <a>
-          <div className="flex flex-col w-full h-full text-center">
-            <img
-              className="rounded-t-lg"
-              width="1280"
-              height="720"
-              alt={`Thumbnail for ${metadata.title}`}
-              src={getYoutubeThumbnailUrl(metadata.youtubeId)}
-            />
-            <div className="bg-slate-50 border-t-2 border-t-violet-600 border-b-[1px] border-b-violet-600 rounded-b-lg overflow-hidden h-full">
-              <Spacer axis="vertical" size={8} />
-              <div className="text-base font-medium">{metadata.title}</div>
-              <Spacer axis="vertical" size={8} />
-              <div className="text-xs font-extralight text-slate-900">{metadata.artist ?? '\u00A0'}</div>
-              <Spacer axis="vertical" size={8} />
-            </div>
+        <div className="flex flex-col w-full h-full text-center">
+          <Image
+            className="rounded-t-lg"
+            width="1280"
+            height="720"
+            alt={`Thumbnail for ${metadata.title}`}
+            src={getYoutubeThumbnailUrl(metadata.youtubeId)}
+          />
+          <div className="bg-slate-50 border-t-2 border-t-violet-600 border-b-[1px] border-b-violet-600 rounded-b-lg overflow-hidden h-full">
+            <Spacer axis="vertical" size={8} />
+            <div className="text-base font-medium">{metadata.title}</div>
+            <Spacer axis="vertical" size={8} />
+            <div className="text-xs font-extralight text-slate-900">{metadata.artist ?? '\u00A0'}</div>
+            <Spacer axis="vertical" size={8} />
           </div>
-        </a>
+        </div>
       </Link>
     </div>
   )
